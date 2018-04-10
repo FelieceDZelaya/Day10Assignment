29e80e3c86d6802c48541deb95e577ec7697ed9f
FelieceZelaya <feliece.zelaya@gmail.com>
2018-04-10 17:12:17 -0600

    Commit

--- build/atmclient.js
+++ build/atmclient.js
@@ -1,10 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const got = require("got");
-console.log("starting ...");
+got.get("http://localhost:3000/atm").then((data) => {
+    console.log("Data from web backend " + data.body);
+}, (err) => {
+    console.log("Error from web backend " + err.message);
+});
+got.get("hhttp://localhost:3000/atm/123456010220001").then((data) => {
+    console.log("Data from web backend " + data.body);
+}, (err) => {
+    console.log("Error from web backend " + err.message);
+});
 got.get("http://localhost:3000/atm/withdraw/123456010220002/amount/200").then((data) => {
     console.log("Data from web backend " + data.body);
-    console.log("finished!");
+}, (err) => {
+    console.log("Error from web backend " + err.message);
+});
+got.get("http://localhost:3000/atm/deposit/123456010220003/amount/200").then((data) => {
+    console.log("Data from web backend " + data.body);
 }, (err) => {
     console.log("Error from web backend " + err.message);
 });
